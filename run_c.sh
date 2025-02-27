ce9eebbc60f8d40911f75e983407a382c685f05e
JIK JHONG <jik.jhong@gmail.com>
2025-02-27 09:20:32 +0800

    Update run_c.sh
    Add function - support using xxx.c or xxx both are fine for compiling in this bash.

--- run_c.sh
+++ run_c.sh
@@ -5,18 +5,37 @@ output_name=${1:-xdpp}
 func_parameter=${2:-}
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 clear
+
+if [[ "$output_name" == *.c ]]; then
+    # 使用者已輸入 xxx.cpp，檢查是否存在
+    if [ ! -f "$output_name" ]; then
+        echo "錯誤：檔案 $output_name 不存在。"
+        exit 1
+    fi
+else
+    # 使用者輸入的是 xxx，檢查 xxx.cpp 是否存在
+    if [ -f "$output_name.c" ]; then
+        output_name="$output_name.c"
+    elif [ ! -f "$output_name" ]; then
+        echo "錯誤：找不到 $output_name 或 $output_name.c。"
+        exit 1
+    fi
+fi
+
+exec_name="${output_name%.c}"
+
 echo ""
 echo "進行作業 $current_time ..."
 echo "正在編譯 $output_name.c ..."
 # 編譯程式，輸出名稱使用變數
-gcc -o "$output_name" "$output_name.c"
+gcc -o "$exec_name" "$output_name"
 if [ $? -eq 0 ]; then
-    echo "編譯成功！執行 $output_name ..."
+    echo "編譯成功！執行 $exec_name ..."
     # 執行編譯後的程式
     # ./"$name"
     echo ""
     # 執行編譯後的程式
-    ./"$output_name" "$func_parameter"
+    ./"$exec_name" "$func_parameter"
     echo ""
     echo "執行完畢 ..."
     echo ""
