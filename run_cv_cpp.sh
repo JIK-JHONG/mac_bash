621b30a782279c2a216bc27ff24088f32ae551cb
JIK JHONG <jik.jhong@gmail.com>
2025-03-04 10:19:03 +0800

    Add files via upload
    Add parameter for - <cpp_version> ; 
    basic command :
    g++ -o <output_name> <output_name>.cpp `pkg-config --cflags --libs opencv4` -std=c++<cpp_version>

--- run_cv_cpp.sh
+++ run_cv_cpp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # 如果沒有提供參數，則使用默認名稱 xdpp
+# g++ -o <output_name> <output_name>.cpp `pkg-config --cflags --libs opencv4` -std=c++<cpp_version>
 # g++ -o ovp ovp.cpp `pkg-config --cflags --libs opencv4` -std=c++11
 output_name=${1:-xdpp}
-func_parameter=${2:-}
+cpp_version=${2:-11}
+func_parameter=${3:-}
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 clear
 
@@ -31,14 +33,20 @@ exec_name="${output_name%.cpp}"
 # echo "gcc 版本： 11"
 echo "-- 編譯版本資訊 --"
 printf "%-15s %s\n" "openCV 版本：" "$(pkg-config --modversion opencv4)"
-printf "%-15s %s\n" "gcc 版本： 11"
+printf "%-15s %s\n" "gcc 版本：" "$(gcc -dumpversion)"
+printf "%-15s %s\n" "使用的 C++ 標準：" "C++$cpp_version"
+echo "--"
+
+echo "進行作業 $current_time ..."
+echo "正在編譯 $exec_name.cpp ..."
+g++ -o "$exec_name" "$output_name" `pkg-config --cflags --libs opencv4` -std=c++"$cpp_version"
 echo "--"
 # printf "%-15s %s\n" "gcc 版本：" "$(gcc -dumpversion)"
 echo "進行作業 $current_time ..."
 echo "正在編譯 $exec_name.cpp ..."
 # 編譯程式，輸出名稱使用變數
 # g++ -o "$output_name" "$output_name.cpp" `pkg-config --cflags --libs opencv4` -std=c++11
-g++ -o "$exec_name" "$output_name" `pkg-config --cflags --libs opencv4` -std=c++11
+g++ -o "$exec_name" "$output_name" `pkg-config --cflags --libs opencv4` -std=c++"$cpp_version"
 if [ $? -eq 0 ]; then
     # echo "編譯成功！執行 $output_name ..."
     # 執行編譯後的程式
